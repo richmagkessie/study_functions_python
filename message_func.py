3a5c9d0257adf194555bdf8bfc49abccdaf31658
Richmond Kessie <kessierich.mag@gmail.com>
2022-10-25 20:52:31 +0000

    Message Printing

---
+++ message_func.py
@@ -0,0 +1,17 @@
+# Functions
+# 1. Void functions
+
+# defining a main function
+def main():
+    print('I have a message for you')
+    message()
+    print('Thank you')
+
+# Function definition
+def message():
+    # print welcome message
+    print('My name is Tokyo City')
+    print('The boys call me TC')
+
+# call the function
+main()
